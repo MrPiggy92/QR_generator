89d34782cea79792293ef3ce18d3242d234f125f
Mr-Piggy <samstuff@samstuff.top>
2024-12-26 13:38:37 +0000

    Updated placement

--- QR.py
+++ QR.py
@@ -7,7 +7,7 @@ A program to generate QR codes
 import data
 import error
 import structure
-#import placement
+import placement
 #import masks
 #import formatting
 #import render
@@ -17,7 +17,9 @@ def generate_QR(data_string):
     binary_data_string = data.encode(data_string) # Encode data with ISO 8859-1
     dec_error_string = error.correction(binary_data_string) # Get Reed-Solomon error codewords for encoded data
     #return dec_error_string
-    return structure.structure(binary_data_string, dec_error_string)
+    full_string = structure.structure(binary_data_string, dec_error_string)
+    layout = placement.place(full_string)
+    return layout
     #return binary_error_string
 
 #-- MAIN --#
